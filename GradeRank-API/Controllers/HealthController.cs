8fec5146326aa84b3934d0d9a9c75f468df1b70a
Ailton <ailton@ontecconsultoria.com.br>
2023-03-26 19:27:50 -0300

    teste publicação

---
+++ GradeRank-API/Controllers/HealthController.cs
@@ -0,0 +1,31 @@
+﻿using GradeRank_Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GradeRank_API.Controllers
+{
+  [Route("api/[controller]")]
+
+  public class HealthController : Controller
+  {
+    private readonly IHealthStatusUseCase _healthStatusUseCase;
+
+    public HealthController(IHealthStatusUseCase healthStatusUseCase)
+    {
+      _healthStatusUseCase = healthStatusUseCase;
+
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetHealthStatus()
+    {
+      var healthStatus = _healthStatusUseCase.GetStatusUseCase();
+      if (healthStatus == null)
+        return NotFound();
+      return Ok(healthStatus);
+    }
+  }
+}
---
+++ GradeRank-Application/UseCases/HealthStatusUseCase.cs
@@ -0,0 +1,22 @@
+﻿using GradeRank_Application.Interfaces;
+using GradeRank_Domain.Models;
+using GradeRank_Domain.Repositories;
+
+namespace GradeRank_Application.UseCases
+{
+  public class HealthStatusUseCase : IHealthStatusUseCase
+  {
+    private readonly IHealthStatusRepository _healthStatusRepository;
+
+    public HealthStatusUseCase(IHealthStatusRepository healthStatusRepository)
+    {
+      _healthStatusRepository = healthStatusRepository;
+    }
+
+    public String GetStatusUseCase() 
+    {
+      string healthstatus = _healthStatusRepository.GetHealthStatus().Result.Status;
+      return healthstatus;
+    }
+  }
+}
