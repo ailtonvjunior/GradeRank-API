943e5d7ae807441b2df1530193c00e17aced9a77
Ailton <ailton@ontecconsultoria.com.br>
2023-03-26 20:11:44 -0300

    Teste para subida aws

--- GradeRank-API/Controllers/HealthController.cs
+++ GradeRank-API/Controllers/HealthController.cs
@@ -23,6 +23,7 @@ namespace GradeRank_API.Controllers
     public async Task<IActionResult> GetHealthStatus()
     {
       var healthStatus = _healthStatusUseCase.GetStatusUseCase();
+      healthStatus = "teste";
       if (healthStatus == null)
         return NotFound();
       return Ok(healthStatus);
